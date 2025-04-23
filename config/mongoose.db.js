8a7943fd517681d3136713518402b4c97a0edda4
Bikash-design-lab <vmodi5425@gmail.com>
2025-04-23 13:18:12 +0530

    Fix error in mongodb

--- config/mongoose.db.js
+++ config/mongoose.db.js
@@ -1,13 +1,31 @@
-const mongoose = require("mongoose")
+const mongoose = require("mongoose");
 
-const ConnectToDB = async()=>{
-    try{
-        await mongoose.connect(process.env.MONGO_URI)
-        console.log("DB connected sucessfully.")
-    }
-    catch(error){
-        console.log("Fails to connect to DB",error)
-    }
-}
+const ConnectToDB = async () => {
+  try {
+    await mongoose.connect(process.env.MONGO_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+    console.log("DB connected successfully.");
+  } catch (error) {
+    console.log("Fails to connect to DB", error.message);
+  }
+};
 
-module.exports = {ConnectToDB}
+module.exports = { ConnectToDB };
+
+
+
+// const mongoose = require("mongoose")
+
+// const ConnectToDB = async()=>{
+//     try{
+//         await mongoose.connect(process.env.MONGO_URI)
+//         console.log("DB connected sucessfully.")
+//     }
+//     catch(error){
+//         console.log("Fails to connect to DB",error)
+//     }
+// }
+
+// module.exports = {ConnectToDB}
